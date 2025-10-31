3aa6013a0dc402ab88d4947645df9ffd71cfce23
Ryan <ryan.white@nrc-cnrc.gc.ca>
2025-10-31 13:44:33 -0400

    fix path for taxonomy checkout.

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -34,7 +34,7 @@ class ValidateDbTestCase(unittest.TestCase):
             "path":"/tmp/miiflask",
             "database":"/tmp/miiflask/miiflask.db",
             "usertables":"/tmp/miiflask/tables_",
-            "measurands": "/tmp/miiflask/resources/MeasurandTaxonomyCatalog.xml",
+            "measurands": "repo/MeasurandTaxonomyCatalog.xml",
             "mlayer": "resources/m-layer",
             "kcdb": "resources/kcdb",
             "kcdb_cmc_data": "kcdb_cmc_physics.json",
