30171ba6d059b3b16e5e13343b1d373822b6fc49
Ryan M White <8209604+ryanmwhitephd@users.noreply.github.com>
2026-05-20 12:18:51 -0400

    Modify entrypoint.sh to use dynamic data directory
    Updated entrypoint script to use APP_DATA_DIR for data directory.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 
 files=$(ls)
 echo "$files"
+DATA_DIR=${APP_DATA_DIR:-/home/data/mii}
+mkdir -p "$DATA_DIR"
 #if [ ! -f data/miiflask.db ]; then
 #  echo "Database file not found. Initializing database..."
 #  python dbinit.py 
 #fi
-sh init.sh /data/mii
+sh init.sh "$DATA_DIR"
 
 echo "Starting Gunicorn..."
 gunicorn --bind 0.0.0.0:8000 -w 1 wsgi
