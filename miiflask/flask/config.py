c06858319aed13d70a6739459d1069ecf93ddfdb
Ryan M White <8209604+ryanmwhitephd@users.noreply.github.com>
2026-05-20 10:24:55 -0400

    Bump version from 0.5.1 to 0.5.2 (#111)

--- miiflask/flask/config.py
+++ miiflask/flask/config.py
@@ -18,7 +18,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ADMIN_SWATCH = "cerulean"
     SECRET_KEY = "secret"
-    VERSION = "0.5.1"
+    VERSION = "0.5.2"
 
 
 class TestingConfig(Config):
