98dc8f761a69613362ddf933827b7e713e2fd4e1
Ryan White <ryan.white@nrc-cnrc.gc.ca>
2026-06-23 13:23:55 -0400

    add kcdb model and modify routes

---
+++ miiflask/flask/kcdb/init.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright © 2026 Ryan Mackenzie White <ryan.white4@canada.ca>
+#
+# Distributed under terms of the Copyright © Her Majesty the Queen in Right of Canada, as represented by the Minister of Statistics Canada, 2019. license.
+
+"""
+
+"""
+from flask import Blueprint
+
+bp = Blueprint("kcdb", __name__)
+
+from miiflask.flask.kcdb import routes
